17782277d4d4f7a79c06669b07af67ee418e7ed9
vandomed <vandomed@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-04-06 18:00:22 +0000

    Added logodds.graph function and made some changes to means.graph.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dvmisc@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RcppExports_old.R
+++ pkg/R/RcppExports_old.R
@@ -46,12 +46,9 @@ interval.groups <- function(x, num = 5, ...) {
 
 # Create graph of mean +/- error bar for continuous variable vs. factor
 means.graph <- function(y, group, error.bars = "t.ci", alpha = 0.05, 
-                        p.legend = TRUE, 
                         plot.list = NULL, 
                         lines.list = NULL, 
-                        axis.list = NULL,
-                        legend.list = NULL, 
-                        ...) {
+                        axis.list = NULL) {
   
   # Drop missing values
   locs.missing <- which(is.na(y) | is.na(group))
@@ -65,75 +62,78 @@ means.graph <- function(y, group, error.bars = "t.ci", alpha = 0.05,
   group.varname <- deparse(substitute(group))
   
   # Get levels of groups variable for tick labels
-  group.levels <- names(table(group))
+  group.levels <- levels(group)
   
   # Get group means
   means <- tapply(X = y, INDEX = group, FUN = mean)
   
   # Create error bars
-  if (error.bars == "sd") {
-    
-    sds <- tapply(X = y, INDEX = group, FUN = sd)
-    lower.bars <- means - sds
-    upper.bars <- means + sds
-    y.label <- paste(y.varname, " (Mean +/- 1 SD)", sep = "")
-    
-  } else if (error.bars == "se") {
-    
-    ses <- tapply(X = y, INDEX = group, FUN = function(x) sd(x) / sqrt(length(x)))
-    lower.bars <- means - ses
-    upper.bars <- means + ses
-    y.label <- paste(y.varname, " (Mean +/- 1 SE)", sep = "")
-    
-  } else if (error.bars == "t.ci") {
-    
-    ns <- tapply(X = y, INDEX = group, FUN = length)
-    sds <- tapply(X = y, INDEX = group, FUN = sd)
-    t.crit <- qt(p = 1 - alpha/2, df = ns - 1)
-    lower.bars <- means - t.crit * sds / sqrt(ns)
-    upper.bars <- means + t.crit * sds / sqrt(ns)
-    y.label <- paste(y.varname, " (Mean +/- 95% CI)", sep = "")
-    
-  } else if (error.bars == "z.ci") {
-    
-    ns <- tapply(X = y, INDEX = group, FUN = length)
-    sds <- tapply(X = y, INDEX = group, FUN = sd)
-    z.crit <- qnorm(p = 1 - alpha/2)
-    lower.bars <- means - z.crit * sds / sqrt(ns)
-    upper.bars <- means + z.crit * sds / sqrt(ns)
-    y.label <- paste(y.varname, " (Mean +/- 95% CI)", sep = "")
-    
-  } else if (error.bars == "none") {
-    
-    lower.bars <- NULL
-    upper.bars <- NULL
-    y.label <- paste(y.varname, " (Mean)", sep = "")
-    
-  }
-  
-  # Figure out ylim values
-  max.error <- max(upper.bars)
-  min.error <- min(lower.bars)
-  span.error <- max.error - min.error
-  y1 <- min.error - 0.1 * span.error
-  y2 <- max.error + 0.1 * span.error
-  
-  # Figure out features of graph, based on user inputs where available
-  plot.list <- list.override(list1 = list(x = means, type = "p", pch = 16, xaxt = "n",
-                                          main = paste("Mean ", y.varname, " by ", group.varname, sep = ""),
-                                          cex.main = 1.25,
-                                          xlab = group.varname, ylab = y.label,
-                                          xlim = c(0.5, length(group.levels) + 0.5),
-                                          ylim = c(y1, y2)),
-                             list2 = plot.list)
-  axis.list <- list.override(list1 = list(side = 1, at = 1: length(group.levels), labels = group.levels),
-                             list2 = axis.list)
-  
-  # Create graph
-  do.call(plot, plot.list)
-  
-  # Add error bars
   if (error.bars != "none") {
+    
+    if (error.bars == "sd") {
+      
+      sds <- tapply(X = y, INDEX = group, FUN = sd)
+      lower.bars <- means - sds
+      upper.bars <- means + sds
+      y.label <- paste(y.varname, " (Mean +/- 1 SD)", sep = "")
+      
+    } else if (error.bars == "se") {
+      
+      ses <- tapply(X = y, INDEX = group, FUN = function(x) sd(x) / sqrt(length(x)))
+      lower.bars <- means - ses
+      upper.bars <- means + ses
+      y.label <- paste(y.varname, " (Mean +/- 1 SE)", sep = "")
+      
+    } else if (error.bars == "t.ci") {
+      
+      ns <- tapply(X = y, INDEX = group, FUN = length)
+      sds <- tapply(X = y, INDEX = group, FUN = sd)
+      t.crit <- qt(p = 1 - alpha/2, df = ns - 1)
+      lower.bars <- means - t.crit * sds / sqrt(ns)
+      upper.bars <- means + t.crit * sds / sqrt(ns)
+      y.label <- paste(y.varname, " (Mean +/- 95% CI)", sep = "")
+      
+    } else if (error.bars == "z.ci") {
+      
+      ns <- tapply(X = y, INDEX = group, FUN = length)
+      sds <- tapply(X = y, INDEX = group, FUN = sd)
+      z.crit <- qnorm(p = 1 - alpha/2)
+      lower.bars <- means - z.crit * sds / sqrt(ns)
+      upper.bars <- means + z.crit * sds / sqrt(ns)
+      y.label <- paste(y.varname, " (Mean +/- 95% CI)", sep = "")
+      
+    }
+    
+    if (error.bars == "none") {
+      
+      lower.bars <- NULL
+      upper.bars <- NULL
+      y.label <- paste(y.varname, " (Mean)", sep = "")
+      
+    }
+    
+    # Figure out ylim values
+    max.error <- max(upper.bars)
+    min.error <- min(lower.bars)
+    span.error <- max.error - min.error
+    y1 <- min.error - 0.1 * span.error
+    y2 <- max.error + 0.1 * span.error
+    
+    # Figure out features of graph, based on user inputs where available
+    plot.list <- list.override(list1 = list(x = means, type = "p", pch = 16, xaxt = "n",
+                                            main = paste("Mean ", y.varname, " by ", group.varname, sep = ""),
+                                            cex.main = 1.25,
+                                            xlab = group.varname, ylab = y.label,
+                                            xlim = c(0.5, length(group.levels) + 0.5),
+                                            ylim = c(y1, y2)),
+                               list2 = plot.list)
+    axis.list <- list.override(list1 = list(side = 1, at = 1: length(group.levels), labels = group.levels),
+                               list2 = axis.list)
+    
+    # Create graph
+    do.call(plot, plot.list)
+    
+    # Add error bars
     for (ii in 1:length(lower.bars)) {
       
       end.points <- c(lower.bars[ii], upper.bars[ii])
@@ -142,43 +142,11 @@ means.graph <- function(y, group, error.bars = "t.ci", alpha = 0.05,
       do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03), y = rep(end.points[2], 2)), lines.list))
       
     }
-  }
-  
-  # Add legend
-  if (p.legend) {
     
-    # Perform t-test/ANOVA
-    if (length(unique(group)) == 2) {
-      pval <- t.test(y ~ group, ...)$p.value
-      if (pval < 0.001) {
-        pval.text <- "T-test P < 0.001"
-      } else if (pval < 0.05) {
-        pval.text <- paste("T-test P = ", sprintf("%.3f", pval), sep = "")
-      } else {
-        pval.text <- paste("T-test P = ", sprintf("%.2f", pval), sep = "")
-      }
-    } else {
-      pval <- summary(aov(y ~ group, ...))[[1]][[1, "Pr(>F)"]]
-      if (pval < 0.001) {
-        pval.text <- "ANOVA P < 0.001"
-      } else if (pval < 0.05) {
-        pval.text <- paste("ANOVA P = ", sprintf("%.3f", pval), sep = "")
-      } else {
-        pval.text <- paste("ANOVA P = ", sprintf("%.2f", pval), sep = "")
-      }
-    }
-    
-    # Add user inputs to legend, if any
-    legend.list <- list.override(list1 = list(x = "topleft", legend = pval.text),
-                                 list2 = legend.list)
-    
-    # Add legend
-    do.call(legend, legend.list)
+    # Add labels
+    do.call(axis, axis.list)
     
   }
-  
-  # Add labels
-  do.call(axis, axis.list)
   
 }
 
@@ -188,11 +156,13 @@ n <- 500
 x <- rnorm(n)
 y <- rbinom(n = n, size = 1, prob = (1 + exp(-0.25 - 0.5 * x))^(-1))
 table(y)
-group <- interval.groups(x)
-group2 <- as.character(group)
-logodds.graph(y = y, group = group, error.bars = "z.ci")
 
+y, group, error.bars = "t.ci", alpha = 0.05, 
+plot.list = NULL, 
+lines.list = NULL, 
+axis.list = NULL)
 
+logodds.graph(y = y, group = group)
 
 logodds.graph <- function(y, group, error.bars = "none", alpha = 0.05,
                           plot.list = NULL, 
@@ -210,33 +180,23 @@ logodds.graph <- function(y, group, error.bars = "none", alpha = 0.05,
   y.varname <- deparse(substitute(y))
   group.varname <- deparse(substitute(group))
   
-  # Create contingency table
-  group.y.table <- table(group, y)
-  
   # Get levels of groups variable for tick labels
-  group.levels <- rownames(group.y.table)
+  group.levels <- levels(group)
   
   # Get outcome probabilities and log-odds for each level
-  probs <- apply(group.y.table, 1, function(x) x[2] / sum(x))
+  probs <- tapply(y, group, mean)
   logodds <- log(probs / (1 - probs))
   
   # Create error bars
   if (error.bars == "exact.ci") {
     
-    probs.ci <- apply(group.y.table, 1, function(x) binom.test(x = rev(x), conf.level = 1 - alpha)$conf.int)
+    y.group.table <- table(group, y)
+    probs.ci <- apply(y.group.table, 1, function(x) binom.test(x = rev(x))$conf.int)
     logodds.ci <- log(probs.ci / (1 - probs.ci))
     lower.bars <- logodds.ci[1, ]
     upper.bars <- logodds.ci[2, ]
     y.label <- paste(y.varname, " (log-odds +/- 95% CI)", sep = "")
     
-  } else if (error.bars == "z.ci") {
-    
-    probs.ci <- apply(group.y.table, 1, function(x) prop.test(x = x[2], n = sum(x), conf.level = 1 - alpha)$conf.int)
-    logodds.ci <- log(probs.ci / (1 - probs.ci))
-    lower.bars <- logodds.ci[1, ]
-    upper.bars <- logodds.ci[2, ]
-    y.label <- paste(y.varname, " (log-odds +/- 95% CI)", sep = "")
-      
   } else if (error.bars == "none") {
     
     lower.bars <- NULL
@@ -274,18 +234,39 @@ logodds.graph <- function(y, group, error.bars = "none", alpha = 0.05,
   do.call(plot, plot.list)
   
   # Add error bars
-  if (error.bars != "none") {
-    for (ii in 1:length(lower.bars)) {
-      
-      end.points <- c(lower.bars[ii], upper.bars[ii])
-      do.call(lines, c(list(x = rep(ii, 2), y = end.points), lines.list))
-      do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03), y = rep(end.points[1], 2)), lines.list))
-      do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03), y = rep(end.points[2], 2)), lines.list))
-      
-    }
+  for (ii in 1:length(lower.bars)) {
+    
+    end.points <- c(lower.bars[ii], upper.bars[ii])
+    do.call(lines, c(list(x = rep(ii, 2), y = end.points), lines.list))
+    do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03), y = rep(end.points[1], 2)), lines.list))
+    do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03), y = rep(end.points[2], 2)), lines.list))
+    
   }
   
   # Add labels
   do.call(axis, axis.list)
   
 }
+  
+  
+  
+  # Plot and return log-odds for y across x groups
+  plot(log.ors, ...)
+  return(list(x = x, y = y, logodds = logodds))
+  
+}
+
+x <- rnorm(10000)
+y <- rbinom(10000, size = 1, prob = (1 + exp(0 - 1 * x))^(-1))
+log.or(x = x, y = y, pch = 16, col = "red")
+
+
+
+
+# interval.groups(x = rnorm(100))
+# 
+# x <- c(rnorm(100), NA, NA, rnorm(100))
+# y <- 0.5 + 1.25 * x + rnorm(202)
+# group <- cut(x, breaks = quantile(x, na.rm = T), include.lowest = T)
+# 
+# means.graph(y = y, group = group)
