edcdcdf0c3a68115a0c496c17a35b16d51126166
vandomed <vandomed@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-07-10 15:41:24 +0000

    Dropped Rcpp dependencies etc. because currently does not contain any C++ functions. Had considered moving general C++ functions in accelerometry and stocks packages into dvmisc, but decided not to at this point. May add C++ functions to dvmisc in the future. Would like to get version 1.1.1 on CRAN soon.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dvmisc@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/functions.R
+++ pkg/R/functions.R
@@ -1,13 +1,14 @@
-# Adds elements of list2 to list1, overwriting any elements with the same name.
+# Adds elements of list2 to list1, overwriting any elements with the same name
 list.override <- function(list1, list2) {
-  
+
   # Get names of elements of list1 and list2
   names.list1 <- names(list1)
   names.list2 <- names(list2)
-  
-  # Loop through elements of list 2. If in list 1, remove, then add; if not in list 1, add.
+
+  # Loop through elements of list 2. If in list 1, remove, then add; if not in
+  # list 1, add.
   for (ii in 1: length(list2)) {
-    
+
     element.name <- names.list2[ii]
     loc.list1 <- which(names.list1 == element.name)
     if (length(loc.list1) > 0) {
@@ -15,68 +16,75 @@ list.override <- function(list1, list2) {
     } else {
       list1 <- c(list1, list2[ii])
     }
-    
+
   }
-  
-  # Return list1, which has its original elements plus any extras/overrides from list2
+
+  # Return list1, which has its original elements plus any extras/overrides from
+  # list2
   return(list1)
-  
+
 }
 
 
 # Check if numeric value is in between two other values
 inside <- function(x, ends, inclusive = TRUE) {
-  
+
   if (inclusive) {
     x >= ends[1] & x <= ends[2]
   } else {
     x > ends[1] & x < ends[2]
   }
-  
+
 }
 
 
 # Create fixed number of groups covering range of input vector x
 interval.groups <- function(x, groups = 5, ...) {
-  
+
   # Figure out break points to split x into even intervals spanning its range
   x.range <- range(x, na.rm = T)
   cut.breaks <- seq(x.range[1], x.range[2], diff(x.range) / groups)
   cut.breaks[c(1, length(cut.breaks))] <- c(-Inf, Inf)
-  
+
   # Create groups
   groups <- cut(x = x, breaks = cut.breaks, ...)
-  
+
   # Print message and return groups
   num.missing <- sum(is.na(groups))
-  message(paste("Observations per group: ", paste(table(groups), collapse = ", "), ". ", num.missing, " missing.", sep = ""))
+  message(paste("Observations per group: ",
+                paste(table(groups), collapse = ", "),
+                ". ", num.missing, " missing.",
+                sep = ""))
   return(groups)
-  
+
 }
 
 
-# Create quantile groups. Consider adding labels option, e.g. could be "#" for number, "Q#" for Q1, Q2, etc., 
-# or "interval" for the actual intervals.
+# Create quantile groups. Consider adding labels option, e.g. could be "#" for
+# number, "Q#" for Q1, Q2, or "interval" for the actual intervals.
 quant.groups <- function(x, groups = 5, ...) {
-  
+
   # Calculate quantiles
   quantiles <- quantile(x, probs = seq(0, 1, 1 / groups), na.rm = T, ...)
-  
+
   # Create quantile groups
   groups <- cut(x, breaks = quantiles, include.lowest = T, ...)
-  
+
   # Print message and return groups
   num.missing <- sum(is.na(groups))
-  message(paste("Observations per group: ", paste(table(groups), collapse = ", "), ". ", num.missing, " missing.", sep = ""))
+  message(paste("Observations per group: ",
+                paste(table(groups), collapse = ", "),
+                ". ", num.missing, " missing.",
+                sep = ""))
   return(groups)
-  
+
 }
 
 
 # Create 3 BMI groups
 bmi3 <- function(x, labels = T) {
   if (labels) {
-    y <- cut(x, breaks = c(-Inf, 25, 30, Inf), right = F, 
+    y <- cut(x, breaks = c(-Inf, 25, 30, Inf), right = F,
              labels = c("Normal weight", "Overweight", "Obese"))
   } else {
     y <- cut(x, breaks = c(-Inf, 25, 30, Inf), right = F)
@@ -88,7 +96,7 @@ bmi3 <- function(x, labels = T) {
 # Create 4 BMI groups
 bmi4 <- function(x, labels = T) {
   if (labels) {
-    y <- cut(x, breaks = c(-Inf, 18.5, 25, 30, Inf), right = F, 
+    y <- cut(x, breaks = c(-Inf, 18.5, 25, 30, Inf), right = F,
              labels = c("Underweight", "Normal weight", "Overweight", "Obese"))
   } else {
     y <- cut(x, breaks = c(-Inf, 18.5, 25, 30, Inf), right = F)
@@ -98,72 +106,73 @@ bmi4 <- function(x, labels = T) {
 
 
 # Create graph of mean +/- error bar for continuous variable vs. factor
-means.graph <- function(y, group, error.bars = "t.ci", alpha = 0.05, 
-                        p.legend = TRUE, 
-                        plot.list = NULL, 
-                        lines.list = NULL, 
+means.graph <- function(y, group, error.bars = "t.ci", alpha = 0.05,
+                        p.legend = TRUE,
+                        plot.list = NULL,
+                        lines.list = NULL,
                         axis.list = NULL,
-                        legend.list = NULL, 
+                        legend.list = NULL,
                         ...) {
-  
+
   # Get name of y and group variables for axis labels
   y.varname <- deparse(substitute(y))
   group.varname <- deparse(substitute(group))
-  
+
   # Drop missing values
   locs.missing <- which(is.na(y) | is.na(group))
   if (length(locs.missing) > 0) {
     y <- y[-locs.missing]
     group <- group[-locs.missing]
   }
-  
+
   # Get levels of groups variable for tick labels
   group.levels <- names(table(group))
-  
+
   # Get group means
   means <- tapply(X = y, INDEX = group, FUN = mean)
-  
+
   # Create error bars
   if (error.bars == "sd") {
-    
+
     sds <- tapply(X = y, INDEX = group, FUN = sd)
     lower.bars <- means - sds
     upper.bars <- means + sds
     y.label <- paste(y.varname, " (Mean +/- 1 SD)", sep = "")
-    
+
   } else if (error.bars == "se") {
-    
-    ses <- tapply(X = y, INDEX = group, FUN = function(x) sd(x) / sqrt(length(x)))
+
+    ses <- tapply(X = y, INDEX = group, FUN = function(x)
+      sd(x) / sqrt(length(x)))
     lower.bars <- means - ses
     upper.bars <- means + ses
     y.label <- paste(y.varname, " (Mean +/- 1 SE)", sep = "")
-    
+
   } else if (error.bars == "t.ci") {
-    
+
     ns <- tapply(X = y, INDEX = group, FUN = length)
     sds <- tapply(X = y, INDEX = group, FUN = sd)
     t.crit <- qt(p = 1 - alpha/2, df = ns - 1)
     lower.bars <- means - t.crit * sds / sqrt(ns)
     upper.bars <- means + t.crit * sds / sqrt(ns)
     y.label <- paste(y.varname, " (Mean +/- 95% CI)", sep = "")
-    
+
   } else if (error.bars == "z.ci") {
-    
+
     ns <- tapply(X = y, INDEX = group, FUN = length)
     sds <- tapply(X = y, INDEX = group, FUN = sd)
     z.crit <- qnorm(p = 1 - alpha/2)
     lower.bars <- means - z.crit * sds / sqrt(ns)
     upper.bars <- means + z.crit * sds / sqrt(ns)
     y.label <- paste(y.varname, " (Mean +/- 95% CI)", sep = "")
-    
+
   } else if (error.bars == "none") {
-    
+
     lower.bars <- NULL
     upper.bars <- NULL
     y.label <- paste(y.varname, " (Mean)", sep = "")
-    
+
   }
-  
+
   # Figure out ylim values
   if (!is.null(lower.bars)) {
     max.error <- max(upper.bars)
@@ -177,36 +186,42 @@ means.graph <- function(y, group, error.bars = "t.ci", alpha = 0.05,
     y1 <- range.means[1] - 0.1 * span.means
     y2 <- range.means[2] + 0.1 * span.means
   }
-  
+
   # Figure out features of graph, based on user inputs where available
-  plot.list <- list.override(list1 = list(x = means, type = "p", pch = 16, xaxt = "n",
-                                          main = paste("Mean ", y.varname, " by ", group.varname, sep = ""),
-                                          cex.main = 1.25,
-                                          xlab = group.varname, ylab = y.label,
-                                          xlim = c(0.5, length(group.levels) + 0.5),
-                                          ylim = c(y1, y2)),
+  plot.list <-
+    list.override(list1 = list(x = means, type = "p", pch = 16, xaxt = "n",
+                               main = paste("Mean ", y.varname, " by ",
+                                            group.varname, sep = ""),
+                               cex.main = 1.25,
+                               xlab = group.varname, ylab = y.label,
+                               xlim = c(0.5, length(group.levels) + 0.5),
+                               ylim = c(y1, y2)),
                              list2 = plot.list)
-  axis.list <- list.override(list1 = list(side = 1, at = 1: length(group.levels), labels = group.levels),
-                             list2 = axis.list)
-  
+  axis.list <-
+    list.override(list1 = list(side = 1, at = 1: length(group.levels),
+                               labels = group.levels),
+                  list2 = axis.list)
+
   # Create graph
   do.call(plot, plot.list)
-  
+
   # Add error bars
   if (error.bars != "none") {
     for (ii in 1:length(lower.bars)) {
-      
+
       end.points <- c(lower.bars[ii], upper.bars[ii])
       do.call(lines, c(list(x = rep(ii, 2), y = end.points), lines.list))
-      do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03), y = rep(end.points[1], 2)), lines.list))
-      do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03), y = rep(end.points[2], 2)), lines.list))
-      
+      do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03),
+                            y = rep(end.points[1], 2)), lines.list))
+      do.call(lines, c(list(x = c(ii - 0.03, ii + 0.03),
+                            y = rep(end.points[2], 2)), lines.list))
+
     }
   }
-  
+
   # Add legend
   if (p.legend) {
-    
+
     # Perform t-test/ANOVA
     if (length(unique(group)) == 2) {
       pval <- t.test(y ~ group, ...)$p.value
@@ -227,86 +242,88 @@ means.graph <- function(y, group, error.bars = "t.ci", alpha = 0.05,
         pval.text <- paste("ANOVA P = ", sprintf("%.2f", pval), sep = "")
       }
     }
-    
+
     # Add user inputs to legend, if any
-    legend.list <- list.override(list1 = list(x = "topleft", legend = pval.text),
+    legend.list <- list.override(list1 = list(x = "topleft",
+                                              legend = pval.text),
                                  list2 = legend.list)
-    
+
     # Add legend
     do.call(legend, legend.list)
-    
+
   }
-  
+
   # Add labels
   do.call(axis, axis.list)
-  
+
 }
 
 
 # Plot sample log-odds for binary variable vs. factor
 logodds.graph <- function(y, group, error.bars = "none", alpha = 0.05,
                           p.legend = "chi",
-                          plot.list = NULL, 
+                          plot.list = NULL,
                           lines.list = NULL,
-                          axis.list = NULL, 
+                          axis.list = NULL,
                           legend.list = NULL,
                           ...) {
-  
+
   # Get name of y and group variables for axis labels
   y.varname <- deparse(substitute(y))
   group.varname <- deparse(substitute(group))
-  
+
   # Drop missing values
   locs.missing <- which(is.na(y) | is.na(group))
   if (length(locs.missing) > 0) {
     y <- y[-locs.missing]
     group <- group[-locs.missing]
   }
-  
+
   # Create contingency table
   group.y.table <- table(group, y)
   x <- 1: nrow(group.y.table)
-  
+
   # Get levels of groups variable for tick labels
   group.levels <- rownames(group.y.table)
-  
+
   # Exclude rows of 0
   locs.0 <- which(apply(group.y.table, 1, sum) == 0)
   if (length(locs.0) > 0) {
     group.y.table <- group.y.table[-locs.0, ]
     x <- x[-locs.0]
   }
-  
+
   # Get outcome probabilities and log-odds for each level
   probs <- apply(group.y.table, 1, function(x) x[2] / sum(x))
   logodds <- log(probs / (1 - probs))
-  
+
   # Create error bars
   if (error.bars == "exact.ci") {
-    
-    probs.ci <- apply(group.y.table, 1, function(x) binom.test(x = rev(x), conf.level = 1 - alpha)$conf.int)
+
+    probs.ci <- apply(group.y.table, 1, function(x)
+      binom.test(x = rev(x), conf.level = 1 - alpha)$conf.int)
     logodds.ci <- log(probs.ci / (1 - probs.ci))
     lower.bars <- logodds.ci[1, ]
     upper.bars <- logodds.ci[2, ]
     y.label <- paste(y.varname, " (log-odds +/- 95% CI)", sep = "")
-    
+
   } else if (error.bars == "z.ci") {
-    
-    probs.ci <- apply(group.y.table, 1, function(x) 
+
+    probs.ci <- apply(group.y.table, 1, function(x)
       prop.test(x = x[2], n = sum(x), conf.level = 1 - alpha)$conf.int)
     logodds.ci <- log(probs.ci / (1 - probs.ci))
     lower.bars <- logodds.ci[1, ]
     upper.bars <- logodds.ci[2, ]
     y.label <- paste(y.varname, " (log-odds +/- 95% CI)", sep = "")
-      
+
   } else if (error.bars == "none") {
-    
+
     lower.bars <- NULL
     upper.bars <- NULL
     y.label <- paste(y.varname, " (log-odds)", sep = "")
-    
+
   }
-  
+
   # Figure out ylim values
   if (!is.null(lower.bars)) {
     max.error <- max(upper.bars[!is.infinite(upper.bars)])
@@ -324,44 +341,50 @@ logodds.graph <- function(y, group, error.bars = "none", alpha = 0.05,
   }
 
   # Figure out features of graph, based on user inputs where available
-  plot.list <- list.override(list1 = list(x = x, y = logodds, 
-                                          type = "p", pch = 16, xaxt = "n",
-                                          main = paste("Log-odds ", y.varname, " by ", group.varname, sep = ""),
-                                          cex.main = 1.25,
-                                          xlab = group.varname, ylab = y.label,
-                                          xlim = c(0.5, length(group.levels) + 0.5),
-                                          ylim = c(y1, y2)),
-                             list2 = plot.list)
-  cex.axis.value <- ifelse(length(group.levels) <= 3, 1, 
+  plot.list <-
+    list.override(list1 = list(x = x, y = logodds,
+                               type = "p", pch = 16, xaxt = "n",
+                               main = paste("Log-odds ", y.varname, " by ",
+                                            group.varname, sep = ""),
+                               cex.main = 1.25,
+                               xlab = group.varname, ylab = y.label,
+                               xlim = c(0.5, length(group.levels) + 0.5),
+                               ylim = c(y1, y2)),
+                  list2 = plot.list)
+  cex.axis.value <- ifelse(length(group.levels) <= 3, 1,
                            ifelse(length(group.levels) >= 8, 0.5,
                                   1 - 0.1 * (length(group.levels) - 3)))
-  axis.list <- list.override(list1 = list(side = 1, at = 1: length(group.levels), labels = group.levels,
-                                          cex.axis = cex.axis.value),
-                             list2 = axis.list)
-  
+  axis.list <-
+    list.override(list1 = list(side = 1, at = 1: length(group.levels),
+                               labels = group.levels,
+                               cex.axis = cex.axis.value),
+                  list2 = axis.list)
+
   # Create graph
   do.call(plot, plot.list)
-  
+
   # Add error bars
   if (error.bars != "none") {
     for (ii in 1:length(lower.bars)) {
-      
+
       end.points <- c(lower.bars[ii], upper.bars[ii])
       do.call(lines, c(list(x = rep(x[ii], 2), y = end.points), lines.list))
-      do.call(lines, c(list(x = c(x[ii] - 0.03, x[ii] + 0.03), y = rep(end.points[1], 2)), lines.list))
-      do.call(lines, c(list(x = c(x[ii] - 0.03, x[ii] + 0.03), y = rep(end.points[2], 2)), lines.list))
-      
+      do.call(lines, c(list(x = c(x[ii] - 0.03, x[ii] + 0.03),
+                            y = rep(end.points[1], 2)), lines.list))
+      do.call(lines, c(list(x = c(x[ii] - 0.03, x[ii] + 0.03),
+                            y = rep(end.points[2], 2)), lines.list))
+
     }
   }
-  
+
   # Add labels
   do.call(axis, axis.list)
-  
+
   # Add legend
   if (p.legend != "none") {
-    
+
     if (p.legend == "chi") {
-      
+
       # Perform Chi-square test for association
       pval <- chisq.test(x = group, y = y, ...)$p.value
       if (pval < 0.001) {
@@ -371,9 +394,9 @@ logodds.graph <- function(y, group, error.bars = "none", alpha = 0.05,
       } else {
         pval.text <- paste("Chi-square P = ", sprintf("%.2f", pval), sep = "")
       }
-      
+
     } else if (p.legend == "fisher") {
-      
+
       # Perform Fisher's exact test
       pval <- fisher.test(x = group, y = y, ...)$p.value
       if (pval < 0.001) {
@@ -383,39 +406,41 @@ logodds.graph <- function(y, group, error.bars = "none", alpha = 0.05,
       } else {
         pval.text <- paste("Fisher P = ", sprintf("%.2f", pval), sep = "")
       }
-      
+
     }
-    
+
     # Add user inputs to legend, if any
-    legend.list <- list.override(list1 = list(x = "topleft", legend = pval.text),
-                                 list2 = legend.list)
-    
+    legend.list <-
+      list.override(list1 = list(x = "topleft", legend = pval.text),
+                    list2 = legend.list)
+
     # Add legend
     do.call(legend, legend.list)
-    
+
   }
-  
+
 }
 
 
-# Versions of which.min and which.max that gives row/column positions for matrices.
+# Versions of which.min and which.max that gives row/column positions for
+# matrices
 which.min2 <- function(x, only.first = FALSE, arr.ind = TRUE) {
-  
+
   loc <- which(x == min(x), arr.ind = arr.ind)
   if (only.first) {
     if (is.matrix(loc)) {
-      loc <- loc[1, , drop = FALSE] 
+      loc <- loc[1, , drop = FALSE]
     } else {
       loc <- loc[1]
     }
   }
   return(loc)
-  
+
 }
 
 
 which.max2 <- function(x, only.first = FALSE, arr.ind = TRUE) {
-  
+
   loc <- which(x == max(x), arr.ind = arr.ind)
   if (only.first) {
     if (is.matrix(loc)) {
@@ -425,9 +450,29 @@ which.max2 <- function(x, only.first = FALSE, arr.ind = TRUE) {
     }
   }
   return(loc)
-  
+
 }
 
 
+# Extract mean squared error from lm or glm model
+get.mse <- function(model.fit, var.estimate = FALSE) {
 
+  # Extract MSE from model.fit
+  class1 <- class(model.fit)[1]
+  if (class1 == "lm") {
+    mse <- rev(anova(model.fit)$"Mean Sq")[1]
+  } else if (class1 == "glm") {
+    mse <- sum(model.fit$residuals^2) / model.fit$df.residual
+  }
 
+  # Get V.hat(MSE) if requested
+  if (var.estimate) {
+    mse.var <- 2 * mse^2 / model.fit$df.residual
+    mse <- c(mse, mse.var)
+    names(mse) <- c("mse", "var")
+  }
+
+  # Return mse variable
+  return(mse)
+
+}
